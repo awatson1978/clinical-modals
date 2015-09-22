60aba94fe6a3d9db7e30270206111014aee90b6e
Abigail Watson <awatson1978@gmail.com>
2015-09-22 15:13:16 -0700

    updated package.js

--- package.js
+++ package.js
@@ -2,9 +2,9 @@ Package.describe({
   name: 'clinical:modals',
   version: '0.0.1',
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'Modal dialogs for the Clinical Meteor track.',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/awatson1978/clinical-modals',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
